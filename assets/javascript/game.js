ab6920c0f56fce77657b3b240b342dfcd8c353fe
sahasra101 <ajaykiri7@gmail.com>
2019-10-19 13:12:12 -0400

    formatting changes to index and gameJS files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,19 +1,20 @@
-// Array of words w vacation hotspot theme
-var words = ["rome", "paris", "newyorkcity", "miami", "orlando", "barcelona", "london", "venice", "dubai", "cancun", "tahiti", "maui", "borabora", "yosemite", "grandcanyon", "machupicchu", "amalficoast", "costarica", "greatbarrierreef", "santorini", "yellowstone", "india"];
-
-console.log(words[0], words[20]); //works
-
-// variables needed for guesses and to keep track of stats
-var wins = 0;
-var losses = 0;
-var numbGuessesWrong = 0;
-var numbGuessesLeft = 10;
-var incorrectArray = [];
-var blankWord = []; // blank word array
-
-
-// Press any key to start javascript
 document.onkeyup = function (event) {
+    // Array of words w vacation hotspot theme
+    var words = ["rome", "paris", "newyorkcity", "miami", "orlando", "barcelona", "london", "venice", "dubai", "cancun", "tahiti", "maui", "borabora", "yosemite", "grandcanyon", "machupicchu", "amalficoast", "costarica", "greatbarrierreef", "santorini", "yellowstone", "india"];
+
+    console.log(words[0], words[20]); //works
+
+    // variables needed for guesses and to keep track of stats
+    var wins = 0;
+    var losses = 0;
+    var word = ""
+    var guessedLetter = "";
+    var numbGuessesWrong = 0;
+    var numbGuessesLeft = 10;
+    var incorrectArray = [];
+    var blankWord = []; // blank word array
+    var playagain = "";
+
 
     //Gaming loop
     //choose random word
@@ -23,15 +24,14 @@ document.onkeyup = function (event) {
     //if correct replace letter index on unknown array with letter
     //if incorrect: add letter to incorrectArray and reduce numbGuessesLeft
     // if user numbGuessesLeft = 0 then player losses game 
-    ask if want to play again
     // if user guesses unknown then congrads you win? would you like to play again
     // keep tally of wins and losses        
 
     //Gaming loop:
-    var gameloop = function() {
+    var gameloop = function () {
         //generate random work from array
-        var word = words[Math.floor(Math.random() * words.length)];
-        console.log(word) //check for success
+        word = words[Math.floor(Math.random() * words.length)];
+        console.log("random word: "+word) //check for success
 
         // generate blank Word Array
         for (var i = 0; i < word.length; i++) {
@@ -42,80 +42,81 @@ document.onkeyup = function (event) {
 
         //obtain userinput and confirm proper input of letter
 
-        document.onkeyup = function uniKeyCode(event) {
-            var k = event.keyCode;
+        guessedLetter = document.getElementById("yourguesses");
+
+        document.onkeyup = function (event) {
+            yourguesses.textContent = event.key;
+          };
+
+        //document.onkeyup = function uniKeyCode(event) {
+        //    var k = event.uniKeyCode;
+        //    console.log(k);
 
             //evaluate for letter
-           if (k>90) || (k<57) {
-                alert("Incorrect key! Please pick a letter from a to z!")
-           } else {
-               var guessedLetter = event.key.toLowerCase()
-               console.log("userGuess: " guessedLetter)
-
-           }               
-
+         //   if (k > 90) || (k < 57) {
+         //       alert("Incorrect key! Please pick a letter from a to z!")
+         //   } else {
+         //       guessedLetter = event.key.toLowerCase();
+         //       console.log("userGuess: " + guessedLetter);
+         //   }
         }
 
-        //   alert("Pick a letter you think is in the unknown vacation hotspot!"); // tell user to pick letter.
-
-        var guessedLetter = prompt("Pick a letter you think is in the unknown vacation hotspot!");
-
-        //  create array of guessed letters to keep track of guesses
-        var guessedLetters = []
-
-        // for (var j = 0; j < numbGuesses; j++)
-        // guessedLetters[j] = "event.key" + ", "
-        // document.onkeyup = function (event) {
-        // guessedLetter.textContent = event.key;
-        //    };
-
-        // if guessedLetter == word[loop through all indices] {
-        // letter replaces underscore in that index
-        // elso the letter inputed placed into array of guessed letter
-        // else numbGuesses increased by one
-        // elso numberGuessesLeft reduced by one
-
-        // when numberGuessesLeft = 0 then prompt("Sorry, you have no more guesses.  Please play again.")
-
-
-        var letterIsMatched = false;
+        //comparing guess with word
+        var letterIsMatched = false; //initially
 
         for (var l = 0; l < word.length; l++) {
             if (word[l] === guessedLetter) {
                 //type the letter into the word index
-                //guessedLetter = blankAnswer[l]
                 blankWord[l] = guessedLetter;
                 letterIsMatched = true;
             }
 
-        }
-        if (!letterIsMatched) {
-
-            incorrectArray.push(guessedLetter);
-            document.getElementById("guessesLeft").textContent = numbGuessesLeft--;
-        }
-        document.getElementById("blankWordTemplate").textContent = blankWord.join(" ");
-        document.getElementById("badguesses").innerHTML = "Incorrect letter guesses: " + incorrectArray;
-
-        console.log("Incorrect letter guesses: " + incorrectArray)
-
-        var done = blankWord.includes("*");
-        console.log(done);
-
-        if (done !== true) {
-            wins++
-            document.getElementById("pwins").textContent = "Wins: " + wins;
-            document.getElementById("plosses").textContent = "Losses: " + losses;
-
-            prompt("Great Job! You Win!!! Would you like to play again? Y for yes / N for no");
-            var continueplaying = event.key
-            if (continueplaying == "y") {
-                refresh()
+            if (!letterIsMatched) {
+                if (incorrectArray.includes(guessedLetter)) {
+                    alert("You have already guessed this letter!");
+                } else {
+                    incorrectArray.push(guessedLetter);
+                    document.getElementById("badguesses").innerHTML = ("Incorrect letter guesses: " + incorrectArray);
+                    console.log("incorrect guesses: " + incorrectArray);
+                    document.getElementById("guessesLeft").textContent = numbGuessesLeft--;
+                    console.log("guesses left: " + numbGuessesLeft);
+                }
             }
-            if (continueplaying == "n") {
-                alert("Thank you for playing! Have a great vacation!");
-            }
-        }
 
-    }
-}
+            if (numbGuessesLeft = 1) {
+                alert("You only have ONE guess left!!");
+            }
+
+            if (numbGuessesLeft = 0) {
+                alert("You have no more guesses!");
+                alert("Please play again!");
+                losses++;
+                document.getElementById("plosses").textContent = "Losses: " + losses;
+                playagain = prompt("Would you like to play again? y(yes) or n(no");
+                if (playagain === "y") {
+                    gameloop();
+                }
+                else if (playagain === "n") {
+                    alert("Thank you and have a nice day!!");
+                    break;
+                }
+            }
+
+            var done = blankWord.includes("*");
+            console.log(done);
+            if (done !== true) {
+                wins++;
+                document.getElementById("pwins").textContent = "Wins: " + wins;
+                playagain = prompt("Great Job! You Win!!! Would you like to play again? Y for yes / N for no");
+                if (playagain == "y") {
+                    gameloop();
+                }
+                if (playagain == "n") {
+                    alert("Thank you for playing! Have a great vacation!");
+                    break;
+                }
+            }
+
+        }
+    }    
+gameloop();
